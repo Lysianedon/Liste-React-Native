b3f182421d8b82c3a527f9afef1aa71fa560c3fe
lysiandon <don.lysiane@gmail.com>
2022-03-01 14:13:54 +0100

    added flag

--- liste/components/Countrycard.js
+++ liste/components/Countrycard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 
 export default function Countrycard(props) {
   return (
     <View style={StyleSheet.container}>
         <Text>Country: {props.country.name.common}</Text>
-        <Text>Capital: {props.country.name.common}</Text>
+        {/* <Text>Capital: {props.country.name.common}</Text> */}
+        <Image style={styles.img} source={{uri : props.country.flags.png}}/>
+        
         {/* {
             props.country.capital[0] === undefined ? ( <Text>Capital : {props.country.capital[0]}</Text>) : (null)
         } */}
@@ -23,5 +25,11 @@ const styles = StyleSheet.create({
     container : {
         borderTop: 1,
         marginBottom: 5,
+        display: 'flex',
+    },
+
+    img : {
+        width: 50,
+        height: 50,
     }
 })
