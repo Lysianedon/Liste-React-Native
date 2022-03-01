904e847b27892326955c0de0aefbc462049334f6
lysiandon <don.lysiane@gmail.com>
2022-03-01 15:02:46 +0100

    toggle capital

--- liste/components/Countrycard.js
+++ liste/components/Countrycard.js
@@ -1,17 +1,36 @@
 import React from 'react'
+import { useContext, useEffect, useState, createContext} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, Pressable } from 'react-native';
 
 export default function Countrycard(props) {
+    const [isDisplayed, setIsDisplayed] = useState("none");
+
+    //
+    const showCityAndWeather = () => {
+
+        setIsDisplayed((prev) => {
+
+            if (prev === "none") {
+                return "block"
+            } else {
+                return "none"
+            }
+        })
+
+    }
   return (
     <View style={StyleSheet.container}>
         <Text>Country: {props.country.name.common}</Text>
+        <Pressable onPress={showCityAndWeather}>
+
+        <Image style={styles.img} source={{uri : props.country.flags.png}} on/>
+        </Pressable>
 
         {
-            props.country.hasOwnProperty('capital') ? ( <Text>Capital : {props.country.capital[0]}</Text>) : (null)
+            props.country.hasOwnProperty('capital') ? ( <Text style={{display : isDisplayed}}>Capital : {props.country.capital[0]}</Text>) : (null)
         }
 
-        <Image style={styles.img} source={{uri : props.country.flags.png}}/>
 
     </View>
   )
@@ -35,5 +54,6 @@ const styles = StyleSheet.create({
         height: 50,
         marginVertical: "15",
         marginHorizontal: 'auto',
-    }
+    },
+
 })
