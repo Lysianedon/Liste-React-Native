5e0c1ff9ceeb421bb5415aa3dce38101bc43e939
lysiandon <don.lysiane@gmail.com>
2022-03-01 18:45:10 +0100

    ras

--- liste/App.js
+++ liste/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import List from './components/List';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text style={styles.texts}>LIST OF COUNTRIES: </Text>
