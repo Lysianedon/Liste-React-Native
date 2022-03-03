fee8015f5bee5393e7760238608bbadf3c6c3c43
lysiandon <don.lysiane@gmail.com>
2022-03-03 02:50:15 +0100

    refactoring state from children to root

--- form/App.js
+++ form/App.js
@@ -1,7 +1,13 @@
+//Imports from React
 import React from 'react';
-import {useState} from "react";
+import {useState, createContext} from "react";
+//Imports from React Native
 import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, TouchableOpacity, Button } from 'react-native';
-
+import { useNavigate, NativeRouter, Routes, Route } from "react-router-native";
+import Homepage from './views/Homepage';
+import Login from './views/Login';
+//Export User Context
+export const UserContext = createContext();
 
 export default function App() {
 
@@ -13,112 +19,44 @@ export default function App() {
     isDisplayed : "none",
   })
 
-  const handleClick = () => {
-    if (password.length >= 6 && email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) && username.name.length > 1) {
-      setLogText("LOG OFF")
-      setUsername( {
-        name : username.name,
-        isDisplayed : "block",
-      })
-  
-    }else {
+  //Image picker
+  const [selectedImage, setSelectedImage] = useState(null);
 
-      alert('Error : fill the fields properly.')
-      setUsername( {
-        name : username.name,
-        isDisplayed : "none",
-      })
-
-      setLogText("LOGIN");
-
-      return;
-    }
+  const value = {
+    email : email,
+    setEmail : setEmail,
+    password : password,
+    setPassword: setPassword,
+    logText : logText,
+    setLogText : setLogText,
+    username : username,
+    setUsername : setUsername,
+    selectedImage : selectedImage,
+    setSelectedImage : setSelectedImage,
 
   }
 
   return (
+    <UserContext.Provider value={value}>
     <SafeAreaView style={styles.container}>
-      <Text style={{display: username.isDisplayed}}>HELLO, {username.name}  ! </Text>
-      <Text style={styles.title}>KONEXIO </Text>
-      {/* LOGO */}
-      <Image style={styles.img}  source={require('./assets/logoform.png')}/>
 
-     {/* USERNAME */}
-
-     <View style={styles.inputView}>
-          <TextInput
-          style={styles.TextInput}
-          placeholder="Your name"
-          placeholderTextColor="#003f5c"
-          onChangeText={(username) => setUsername({
-            name : username,
-            isDisplayed : "none",
-          })}
-          />
-      </View>
-
-    {/* EMAIL INPUT */}
-    {
-      email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) ? 
-
-      ( <View style={styles.inputViewSuccess}>
-          <TextInput
-          style={styles.TextInput}
-          placeholder="Your email"
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-          />
-      </View>)
-      :
-      ( <View style={styles.inputView}>
-          <TextInput
-          style={styles.TextInput}
-          placeholder="Your email"
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-          />
-      </View>)
-    }
-
-      {/* PASSWORD INPUT */}
-      {
-          password.length >= 6 ?
-
-          ( <View style={styles.inputViewSuccess}>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="Your password"
-              placeholderTextColor="#003f5c"
-              secureTextEntry={true}
-              onChangeText={(password) => setPassword(password)}
-              />
-            </View>)
-            :
-
-            (<View style={styles.inputView}>
-              <TextInput
-                style={styles.TextInput}
-                placeholder="Your password"
-                placeholderTextColor="#003f5c"
-                secureTextEntry={true}
-                onChangeText={(password) => setPassword(password)}
-                />
-              </View>)
-    
-        }
-
-      <TouchableOpacity style={styles.loginBtn} onPress={handleClick}>
-      <Text style={styles.loginText}>{logText}</Text> 
-      </TouchableOpacity>
+      <NativeRouter>
+        <Routes>
+          <Route exact path="/" element={<Login/>}/>
+          <Route exact path="/homepage" element={<Homepage/>}/>
+        </Routes>
+      </NativeRouter>
 
     </SafeAreaView>
+    </UserContext.Provider>
   );
 }
+
+// ----------------- STYLE COMPONENTS  --------------------
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     borderLeftWidth : 11,
@@ -126,84 +64,11 @@ const styles = StyleSheet.create({
     borderTopWidth : 10,
     borderBottomWidth : 10,
     borderRadius: 10,
+    height: "100%",
+    width: "100%",
 
   },
-  title : {
-    fontSize: 40,
-    marginBottom: 45,
-    fontWeight: "bold",
-  },
-
-  img: {
-    width: 70,
-    height: 110,
-    marginBottom: 50,
-  },
-
-  inputView : {
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
-    borderColor: "black",
-    borderLeftWidth: 3,
-    borderRightWidth: 3,
-    borderTopWidth: 2,
-    borderBottomWidth: 2,
-  },
-
-  inputViewError : {
-    borderColor: "red",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
-    borderLeftWidth: 3,
-    borderRightWidth: 3,
-    borderTopWidth: 2,
-    borderBottomWidth: 2,
-
-  },
-
-  inputViewSuccess : {
-    borderColor: "green",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
-    borderLeftWidth: 3,
-    borderRightWidth: 3,
-    borderTopWidth: 2,
-    borderBottomWidth: 2,
-  },
-  
-  TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 5,
-  },
-
-  loginBtn:
-  {
-   width:"80%",
-   borderRadius:25,
-   height:50,
-   alignItems:"center",
-   justifyContent:"center",
-   marginTop:40,
-   backgroundColor:"black",
-
-  },
-
-  loginText : {
-    color: "white",
-    fontWeight: "bold", 
-  }
-
-
 
 });
+
+
