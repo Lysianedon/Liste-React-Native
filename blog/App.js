3a63d23db8a1a9d8542335dcfda002f26eef89d9
lysiandon <don.lysiane@gmail.com>
2022-03-03 14:44:24 +0100

    created routing

--- blog/App.js
+++ blog/App.js
@@ -1,15 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { useNavigate, NativeRouter, Routes, Route } from "react-router-native";
+//Imports from React
+import React from 'react';
+import {useState, createContext} from "react";
+// Import Views
+import Login from './views/Login';
+import Home from './views/Home';
+import Profile from './views/Profile';
+import Comments from './views/Comments';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <NativeRouter>
+        <Routes>
+          <Route exact path="/" element={Login}></Route>
+          <Route exact path="/home" element={Home}></Route>
+          <Route exact path="/profile" element={Profile}></Route>
+          <Route exact path="/comments" element={Comments}></Route>
+        </Routes>
+      </NativeRouter>
+
+    </SafeAreaView>
   );
 }
 
+
+
+
+// ----------------- STYLE COMPONENTS  --------------------
 const styles = StyleSheet.create({
   container: {
     flex: 1,
